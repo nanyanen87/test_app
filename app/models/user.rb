4f65602990f56ae9df94eefa0824c10686bab519
nan hanaoka <nan.hanaoka@gmail.com>
2017-11-07 13:20:49 +0000

    中途半端完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, presence: true, length: { maximum: 255 }
   
-  has_secure_password
+  # has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'articles#index'
   
   resources :articles
   
-  root 'articles#index'
+  resources :users
   
   get  '/signup',  to: 'users#new'
+  
+  
+  
   
 end
