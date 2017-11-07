d1e0d4b11aaa461639cd7c3225109f1e00692ee3
nan hanaoka <nan.hanaoka@gmail.com>
2017-11-07 11:07:03 +0000

    Add actionCreate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,8 +12,18 @@ class ArticlesController < ApplicationController
   end
   
   def create
+    @article = Article.new(article_params)
+    if @article.save
+    redirect_to articles_path
+    else
+      render 'new'
+    end
   end
   
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
   
   
   
