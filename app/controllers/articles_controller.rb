ef0614df4d6273ee52b7d96171e0d0ed108b92fb
nan hanaoka <nan.hanaoka@gmail.com>
2017-11-07 10:21:03 +0000

    Add actionShow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all.order(created_at: 'desc')
   end
   
+  def show
+    @article = Article.find(params[:id])
+  end 
+  
   
   
   
