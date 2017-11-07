79ba2d9bffa7b735c7bb4962b6c36b52fae50392
nan hanaoka <nan.hanaoka@gmail.com>
2017-11-07 12:47:15 +0000

    Make a basic User model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :email, presence: true
+  validates :email, presence: true, length: { maximum: 255 }
+  
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
